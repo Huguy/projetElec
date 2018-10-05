cf7954cddc58474b55e5dffea22e085ec6601013
Huguy <hugo.cr9@hotmail.fr>
2018-10-05 13:35:04 +0200

    Finalisation de l'application pour les fonctions de base.

---
+++ app/src/main/java/com/example/user/projetelec/JoystickController.java
@@ -0,0 +1,278 @@
+package com.example.user.projetelec;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.os.AsyncTask;
+import java.io.IOException;
+import java.util.UUID;
+import com.example.user.projetelec.R;
+
+
+public class JoystickController extends AppCompatActivity implements JoystickView.JoystickListener {
+
+    JoystickView joy1;
+    JoystickView joy2;
+    Button btnDis;
+    String address = null;
+    private ProgressDialog progress;
+    BluetoothAdapter myBluetooth = null;
+    BluetoothSocket btSocket = null;
+    private boolean isBtConnected = false;
+    //SPP UUID. Look for it
+    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        //Intent newint = getIntent();
+        //address = newint.getStringExtra(MainActivity.EXTRA_ADDRESS); //receive the address of the bluetooth device
+
+        //view of the ledControl
+        setContentView(R.layout.activity_joystick_controller);
+
+        //call the widgtes
+        btnDis = findViewById(R.id.button);
+        joy1=findViewById(R.id.Joy1);
+        joy2=findViewById(R.id.Joy2);
+
+
+        //new ConnectBT().execute(); //Call the class to connect
+
+
+
+        btnDis.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Disconnect(); //close connection
+            }
+        });
+
+    }
+
+    @Override
+    public void onJoystickMoved(float xPercent, float yPercent, int id){
+        switch (id){
+            case R.id.Joy1:
+                Log.d("Left", "Xpercent : " + xPercent + " Y percent : " + yPercent);
+                leftJoystick(xPercent, yPercent);
+
+            case R.id.Joy2:
+                Log.d("Right", "Xpercent : " + xPercent + " Y percent : " + yPercent);
+                rightJoystick(xPercent, yPercent);
+
+        }
+
+    }
+
+    private void Disconnect()
+    {
+        if (btSocket!=null) //If the btSocket is busy
+        {
+            try
+            {
+                btSocket.close(); //close connection
+            }
+            catch (IOException e)
+            { msg("Error");}
+        }
+        finish(); //return to the first layout
+
+    }
+
+
+
+    // fast way to call Toast
+    private void msg(String s)
+    {
+        Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_led_control, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void leftJoystick(float xPercent, float yPercent){
+    //    if (btSocket!=null)
+    //    {
+            if ((xPercent>0.2) && (yPercent<0.5) && (yPercent>-0.5)){
+                //try
+                //{
+                    //btSocket.getOutputStream().write("1D".toString().getBytes());
+                    Log.d("1D", "1D");
+                //}
+               // catch (IOException e)
+               // {
+              //      msg("Error");
+               // }
+            }
+            else if ((xPercent<-0.2) && (yPercent<0.5) && (yPercent>-0.5)){
+                Log.d("1G", "1G");
+                //try
+                //{
+                //    btSocket.getOutputStream().write("1G".toString().getBytes());
+               // }
+                //catch (IOException e)
+               // {
+               //     msg("Error");
+               // }
+            }
+            else if ((yPercent<-0.2) && (xPercent<0.5) &&(xPercent>-0.5)){
+                Log.d("1H", "1H");
+                /*try
+                {
+                    btSocket.getOutputStream().write("1H".toString().getBytes());
+                }
+                catch (IOException e)
+                {
+                    msg("Error");
+                }*/
+            }
+            else if ((yPercent>0.2) && (xPercent<0.5) &&(xPercent>-0.5)){
+                Log.d("1B", "1B");
+                /*try
+                {
+                    btSocket.getOutputStream().write("1B".toString().getBytes());
+                }
+                catch (IOException e)
+                {
+                    msg("Error");
+                }*/
+            }
+
+       // }
+    }
+
+    public void rightJoystick(float xPercent, float yPercent){
+        if (btSocket!=null)
+        {
+            if ((xPercent>0.2) && (yPercent<0.5) && (yPercent>-0.5)){
+                try
+                {
+                    btSocket.getOutputStream().write("2D".toString().getBytes());
+                }
+                catch (IOException e)
+                {
+                    msg("Error");
+                }
+            }
+            else if ((xPercent<-0.2) && (yPercent<0.5) && (yPercent>-0.5)){
+                try
+                {
+                    btSocket.getOutputStream().write("2G".toString().getBytes());
+                }
+                catch (IOException e)
+                {
+                    msg("Error");
+                }
+            }
+            else if ((yPercent>0.2) && (xPercent<0.5) &&(xPercent>-0.5)){
+                try
+                {
+                    btSocket.getOutputStream().write("2H".toString().getBytes());
+                }
+                catch (IOException e)
+                {
+                    msg("Error");
+                }
+            }
+            else if ((yPercent<-0.2) && (xPercent<0.5) &&(xPercent>-0.5)){
+                try
+                {
+                    btSocket.getOutputStream().write("2B".toString().getBytes());
+                }
+                catch (IOException e)
+                {
+                    msg("Error");
+                }
+            }
+
+        }
+    }
+
+    private class ConnectBT extends AsyncTask<Void, Void, Void>  // UI thread
+    {
+        private boolean ConnectSuccess = true; //if it's here, it's almost connected
+
+        @Override
+        protected void onPreExecute()
+        {
+            progress = ProgressDialog.show(JoystickController.this, "Connecting...", "Please wait!!!");  //show a progress dialog
+        }
+
+        @Override
+        protected Void doInBackground(Void... devices) //while the progress dialog is shown, the connection is done in background
+        {
+            try
+            {
+                if (btSocket == null || !isBtConnected)
+                {
+                    myBluetooth = BluetoothAdapter.getDefaultAdapter();//get the mobile bluetooth device
+                    BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(address);//connects to the device's address and checks if it's available
+                    btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);//create a RFCOMM (SPP) connection
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                    btSocket.connect();//start connection
+                }
+            }
+            catch (IOException e)
+            {
+                ConnectSuccess = false;//if the try failed, you can check the exception here
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result) //after the doInBackground, it checks if everything went fine
+        {
+            super.onPostExecute(result);
+
+            if (!ConnectSuccess)
+            {
+                msg("Connection Failed. Is it a SPP Bluetooth? Try again.");
+                finish();
+            }
+            else
+            {
+                msg("Connected.");
+                isBtConnected = true;
+            }
+            progress.dismiss();
+        }
+
+
+    }
+}
--- app/src/main/java/com/example/user/projetelec/JoystickView.java
+++ app/src/main/java/com/example/user/projetelec/JoystickView.java
@@ -6,11 +6,9 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
 
 public class JoystickView extends SurfaceView implements SurfaceHolder.Callback {
 
@@ -19,6 +17,7 @@ public class JoystickView extends SurfaceView implements SurfaceHolder.Callback 
     public float baseRadius;
     public float hatRadius;
     private JoystickListener joystickCallBack;
+    private final int ratio=5;
 
     private void setupDimensions(){
         centerX = getWidth()/2;
@@ -56,10 +55,25 @@ public class JoystickView extends SurfaceView implements SurfaceHolder.Callback 
             Canvas myCanvas = this.getHolder().lockCanvas();
             Paint colors = new Paint();
             myCanvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
-            colors.setARGB(255,50,50,50);
+
+
+            float hypotenuse = (float) Math.sqrt(Math.pow(newX - centerX, 2) + Math.pow(newY - centerY, 2));
+            float sin = (newY - centerY)/hypotenuse;
+            float cos = (newX - centerX)/hypotenuse;
+
+            colors.setARGB(255,100,100,100);
             myCanvas.drawCircle(centerX, centerY, baseRadius, colors);
-            colors.setARGB(255,0,0,255);
-            myCanvas.drawCircle(newX, newY, hatRadius, colors);
+
+
+            for (int i = 1; i <= (int) (baseRadius/ratio); i++){
+                colors.setARGB(150/i, 255,0,0);
+                myCanvas.drawCircle(newX - cos * hypotenuse * (ratio / baseRadius) * i, newY - sin * hypotenuse * (ratio/baseRadius) * i, i * (hatRadius * ratio/baseRadius), colors);
+            }
+
+            for (int i = 0; i <= (int) (hatRadius/ratio); i++){
+                colors.setARGB(255, (int) (i * (255 * ratio/hatRadius)), (int) (i * (255 * ratio/hatRadius)), 255);
+                myCanvas.drawCircle(newX, newY, hatRadius - (float) i * (ratio) / 2, colors);
+            }
             getHolder().unlockCanvasAndPost(myCanvas);
         }
     }
